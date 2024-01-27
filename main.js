4ac206e456042cbf7745301ce87197cbea8e7f7a
b <vpbalint@silentsignal.hu>
2024-01-27 18:59:43 +0100

    old compatibility

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const { app, BrowserWindow,ipcMain } = require('electron')
-const path = require('node:path')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
     }
   })
 
